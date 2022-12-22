dabda523f6ffc2b2f69a8003b2abcd5b2eb50954
surendra11reddy <120101715+surendra11reddy@users.noreply.github.com>
2022-12-22 19:15:37 +0530

    Update build.sh

--- bin/build.sh
+++ bin/build.sh
@@ -1,3 +1,3 @@
 echo "hello world"
 
-curl -u venkatasurendrareddy.pydela@cognizant.com:KavithaChandrahas@11 -F "data=@./src/apis/$1.zip" "https://env548989.apigw-aw-us.webmethods.io/rest/apigateway/archive?overwrite=apis,applications,policies,policyactions,aliases&fixingMissingVersions=true"
+curl -u venkatasurendrareddy.pydela@cognizant.com:KavithaChandrahas@11 -F "data=@$2/$1.zip" "https://env548989.apigw-aw-us.webmethods.io/rest/apigateway/archive?overwrite=apis,applications,policies,policyactions,aliases&fixingMissingVersions=true"
