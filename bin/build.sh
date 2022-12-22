181a29c65bb921d172bb28d97db4dfba50826550
surendra11reddy <120101715+surendra11reddy@users.noreply.github.com>
2022-12-22 12:09:07 +0530

    Update build.sh

--- bin/build.sh
+++ bin/build.sh
@@ -1,1 +1,3 @@
 echo "hello world"
+
+curl -u venkatasurendrareddy.pydela@cognizant.com:KavithaChandrahas@11 -F "data=@./ATG_Call_wMInt_Service.zip" "https://env548989.apigw-aw-us.webmethods.io/rest/apigateway/archive?overwrite=apis,applications,policies,policyactions,aliases&fixingMissingVersions=true"
