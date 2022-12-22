7f7c33310527ee09169d4e71cc3ffe1ad31a80f0
surendra11reddy <120101715+surendra11reddy@users.noreply.github.com>
2022-12-22 18:31:49 +0530

    Update build.sh

--- bin/build.sh
+++ bin/build.sh
@@ -1,5 +1,3 @@
 echo "hello world"
 
-declare api_name = "PetStore"
-
-curl -u venkatasurendrareddy.pydela@cognizant.com:KavithaChandrahas@11 -F "data=@./src/apis/$api_name.zip" "https://env548989.apigw-aw-us.webmethods.io/rest/apigateway/archive?overwrite=apis,applications,policies,policyactions,aliases&fixingMissingVersions=true"
+curl -u venkatasurendrareddy.pydela@cognizant.com:KavithaChandrahas@11 -F "data=@./src/apis/$1.zip" "https://env548989.apigw-aw-us.webmethods.io/rest/apigateway/archive?overwrite=apis,applications,policies,policyactions,aliases&fixingMissingVersions=true"
